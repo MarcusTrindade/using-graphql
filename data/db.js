6f34316f4974b5a59fada617038e22c1d5e2fa1f
MarcusTrindade <marcao9a22@gmail.com>
2020-01-22 20:31:39 -0300

    Adicionado um tipo enum para exibir o status do usuario

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const perfis = [
-  {id: 1, nome: 'Comum'}, 
-  {id: 2, nome: 'Administrador'}
+  { id: 1, nome: 'Comum' }, 
+  { id: 2, nome: 'Administrador' }
 ]
 
 const usuarios = [{
@@ -8,19 +8,22 @@ const usuarios = [{
   nome: 'João Silva',
   email: 'jsilva@zemail.com',
   idade: 29,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'ATIVO'
 }, {
   id: 2,
   nome: 'Marcus Trindade',
   email: 'marcao@zemail.com',
   idade: 20,
-  perfil_id: 2
+  perfil_id: 2,
+  status: 'INATIVO'
 }, {
   id: 3,
   nome: 'Rafael Martini',
   email: 'rafael@zemail.com',
   idade: 33,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'BLOQUEADO'
 }]
 
 module.exports = { usuarios, perfis }
